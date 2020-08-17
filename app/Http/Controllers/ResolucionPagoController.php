b6c651f3130d8f2da2ea8812e85f54b6f1e8a5b6
joosee-paabloo <pablo_jose12@hotmail.es>
2020-08-17 11:22:16 -0600

    correccion de errores AP

--- app/Http/Controllers/ResolucionPagoController.php
+++ app/Http/Controllers/ResolucionPagoController.php
@@ -52,7 +52,7 @@ class ResolucionPagoController extends Controller
 
     public function reporte_ap(PlataformaSolicitudAp $id)
     {
-        
+
         $path = 'images/timbre.png';
         $data = file_get_contents($path);
         $base64 = 'data:image/' . "png" . ';base64,' . base64_encode($data);
@@ -61,7 +61,7 @@ class ResolucionPagoController extends Controller
 
         $query = "SELECT U.id, U.no_solicitud, U.n_colegiado, AP.Nombre1, S.estado_solicitud_ap, U.no_cuenta, U.fecha_pago_ap
             FROM sigecig_solicitudes_ap U
-            INNER JOIN sigecig_estado_solicitud_ap S ON U.id_estado_solicitud=S.id 
+            INNER JOIN sigecig_estado_solicitud_ap S ON U.id_estado_solicitud=S.id
             INNER JOIN adm_usuario AU ON AU.Usuario=U.n_colegiado
             INNER JOIN adm_persona AP ON AU.idPersona = AP.idPersona
             INNER JOIN sigecig_tipo_cuentas TC ON TC.id=U.id_tipo_cuenta
@@ -264,7 +264,7 @@ class ResolucionPagoController extends Controller
 
             return Response::json(['success' => 'Éxito']);
         }
-       
+
     }
 
     /**
@@ -286,11 +286,11 @@ class ResolucionPagoController extends Controller
         );
         $json = json_encode($nuevos_datos);
         $solicitud->update($nuevos_datos);
-    
+
             event(new ActualizacionBitacoraAp(Auth::user()->id, $solicitud->id, $fecha, $solicitud->id_estado_solicitud));
             //return redirect()->route('tipoDePago.index', $tipo)->with('flash','Tipo de pago ha sido actualizado!');
             return Response::json(['success' => 'Éxito']);
-       
+
 
     }
 
@@ -299,7 +299,7 @@ class ResolucionPagoController extends Controller
         $data = $request->all();
 
         Mail::send('mails.cambioestado', ['data' => $data],  function ($m) use ($data) {
-            $m->from('visa@cig.org.gt', 'Colegio de Ingenieros de Guatemala');
+            $m->from('visa@cig.org.gt', 'Colegio de Ingenieros de Guatemala portal electrónico');
             $m->to("ing.ivargas21314@gmail.com", "Iver Vargas")->subject('Prueba de Correo');
         });
 
@@ -326,12 +326,12 @@ class ResolucionPagoController extends Controller
             ->join('cc00prof', 'cc00.c_cliente', '=', 'cc00prof.c_cliente')
             ->whereIn('cc00.c_cliente', $cuenta)->orderBy('c_cliente','asc')
             ->get();
-       
-        $List = implode(', ', $cuenta); 
-        
+
+        $List = implode(', ', $cuenta);
+
         $query = "SELECT CONVERT(INT, U.c_cliente) as cliente, U.n_cliente, U.registro, S.n_profesion, U.telefono, U.fecha_nac, U.f_ult_pago, U.f_ult_timbre
         FROM cc00 U
-        INNER JOIN cc00prof S ON U.c_cliente=S.c_cliente 
+        INNER JOIN cc00prof S ON U.c_cliente=S.c_cliente
         WHERE  U.c_cliente IN ($List)
         ORDER BY cliente asc;";
 
@@ -377,7 +377,7 @@ class ResolucionPagoController extends Controller
         $estado_solicitud = PlataformaSolicitudAp::Where("id", $request->solicitud)->get()->first();
         $estado_solicitud->id_estado_solicitud = '4';
         $estado_solicitud->update();
-      
+
             //envio de Corroe Aprobacion Documentacion
             $fecha_actual = date_format(Now(), 'd-m-Y');
             $solicitudAP = PlataformaSolicitudAp::Where("id", $request->solicitud)->get()->first();
@@ -388,7 +388,7 @@ class ResolucionPagoController extends Controller
 
             event(new ActualizacionBitacoraAp(Auth::user()->id, $estado_solicitud->id, $fecha, $estado_solicitud->id_estado_solicitud));
             return response()->json(['mensaje' => 'Resgistrado Correctamente']);
-     
+
 
     }
 
@@ -416,7 +416,7 @@ class ResolucionPagoController extends Controller
             event(new ActualizacionBitacoraAp(Auth::user()->id, $estado_solicitud->id, $fecha, $estado_solicitud->id_estado_solicitud));
             return response()->json(['mensaje' => 'Registrado Correctamente']);
         }
-       
+
     }
 
     public function aprDocumentosJunta(Request $request)
@@ -425,7 +425,7 @@ class ResolucionPagoController extends Controller
         $estado_solicitud = PlataformaSolicitudAp::Where("id", $request->id_solicitud)->get()->first();
         $estado_solicitud->id_estado_solicitud = '5';
         $estado_solicitud->update();
-     
+
             //envio de Corroe Aprobacion Solicitud por Junta Directiva
             $fecha_actual = date_format(Now(), 'd-m-Y');
             $solicitudAP = PlataformaSolicitudAp::Where("id",  $request->id_solicitud)->get()->first();
@@ -436,7 +436,7 @@ class ResolucionPagoController extends Controller
 
             event(new ActualizacionBitacoraAp(Auth::user()->id, $estado_solicitud->id, $fecha, $estado_solicitud->id_estado_solicitud));
             return response()->json(['mensaje' => 'Resgistrado Correctamente']);
-     
+
     }
 
     public function rczDocumentosJunta(Request $request)
@@ -455,7 +455,7 @@ class ResolucionPagoController extends Controller
             $infoCorreoAp = new \App\Mail\AprobacionDocAp($fecha_actual, $solicitudAP, $colegiado);
             $infoCorreoAp->subject('Solicitud de Auxilio Póstumo ' . $solicitudAP->no_solicitud);
             Mail::to($colegiado->e_mail)->send($infoCorreoAp);
-    
+
             event(new ActualizacionBitacoraAp(Auth::user()->id, $estado_solicitud->id, $fecha, $estado_solicitud->id_estado_solicitud));
             return response()->json(['mensaje' => 'Resgistrado Correctamente']);
         } catch (\Throwable $th) {
@@ -473,13 +473,13 @@ class ResolucionPagoController extends Controller
         $type = File::mimeType($path);
         $response = Response::make($file, 200);
         $response->header("Content-Type", $type);
- 
+
        $data = file_get_contents($path);
        $base64 = 'data:doc/' . "pdf" . ';base64,' . base64_encode($data);
-       return $base64; 
-       
+       return $base64;
+
         return $base64;
-      
+
     }
     public function verDpiAp($solicitud)
     {
