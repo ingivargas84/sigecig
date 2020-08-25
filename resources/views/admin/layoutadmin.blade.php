20b431ce946b31e014cba23d7891f07f1d6ba128
DARIO ELIAS <cig.desarrollo2@gmail.com>
2020-08-24 21:09:00 -0600

    RE-47 Reopen

--- resources/views/admin/layoutadmin.blade.php
+++ resources/views/admin/layoutadmin.blade.php
@@ -15,8 +15,6 @@
   <script src="{{asset('js/jquery-ui.min.js')}}"></script>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('font-awesome-4.5.0/css/font-awesome.min.css')}}">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="{{asset('ionicons-2.0.1/css/ionicons.min.css')}}">
    <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Muli">
   <link rel="stylesheet" href="{{asset('css/style.css') }}">
@@ -29,7 +27,9 @@
   <link rel="stylesheet" href="/ea/jquery-ui.css"/>
 
   <!-- Fonts -->
-	{{-- <link href='/ea/font' rel='stylesheet' type='text/css'> --}}
+  {{-- <link href='/ea/font' rel='stylesheet' type='text/css'> --}}
+     <!-- Ionicons -->
+     <link rel="stylesheet" href="{{asset('ionicons-2.0.1/css/ionicons.min.css')}}">
 
 
 
